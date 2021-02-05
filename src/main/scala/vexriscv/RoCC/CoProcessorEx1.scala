7835496fc99682608a421c32d8b5885823d9e55c
GuitarZeiZei <213173648@seu.edu.cn>
2021-02-05 13:09:22 +0800

    Modify instruction pattern

--- src/main/scala/vexriscv/RoCC/CoProcessorEx1.scala
+++ src/main/scala/vexriscv/RoCC/CoProcessorEx1.scala
@@ -13,7 +13,6 @@ class CoProcessorEx1 extends CoProcessor {
 
   case class Cmd() extends InputBundle {
     val funct = Bits(7 bits)
-    //val intRomAddr = Bits(5 bits) // Internal RAM Addr
     val rs2 = Bits(5 bits)
     val rs1 = Bits(5 bits)
     val xd = Bool()
@@ -21,14 +20,6 @@ class CoProcessorEx1 extends CoProcessor {
     val xs2 = Bool()
     val rd = Bits(5 bits)
     val opcode = Bits(7 bits)
-
-    /*val opcode = Bits(7 bits)
-    val rd = Bits(5 bits)
-    val funct3 = Bits(3 bits)
-    val rs1 = Bits(5 bits)
-    val rs2 = Bits(5 bits)
-    val intRomAddr = Bits(5 bits) // Internal RAM Addr
-    val funct2 = Bits(2 bits)*/
   }
 
   case class Rsp() extends OutputBundle {
@@ -36,23 +27,17 @@ class CoProcessorEx1 extends CoProcessor {
   }
 
   def ex1 = new InstructionFunction[Cmd, Rsp](new Cmd(), new Rsp()) {
-    val pattern: String = s"00---------------001-----${custom0}"
+    val pattern: String = s"0000001----------110-----${custom0}"
     val description: String = "Example Coprocessor"
 
     def build(controller: EventController): Unit = {
 
       val regs = controller.prepare event new Area {
         def init = List (0x20, 0x40, 0x60)
-        //val internalRom = Mem(Bits(8 bits), init.map(B(_ , 8 bits)))
       }
 
       val exec = controller.exec event new Area {
         val counter = Counter(50)
-
-        //val romReadAddr = command.intRomAddr
-          //.asUInt.resize(2)
-        //val romRead = regs.internalRom
-          //.readAsync(romReadAddr)
         response.data := (command.cpuRS1.asUInt + command.cpuRS2.asUInt).asBits
 
         when(counter.willOverflowIfInc) {

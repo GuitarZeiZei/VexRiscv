2be6ee23e677b221f14f75cc039576f606560bad
GuitarZeiZei <213173648@seu.edu.cn>
2021-04-13 16:54:11 +0800

    Add Float SUB

--- src/main/scala/vexriscv/RoCC/CoProcessorEx1.scala
+++ src/main/scala/vexriscv/RoCC/CoProcessorEx1.scala
@@ -1,6 +1,6 @@
 /**
   * Created on 2021/3/24
-  * Instantiation of a MAC coprocessor
+  * Instantiation of a Float Cal coprocessor
   */
 
 package vexriscv.RoCC
@@ -30,7 +30,7 @@ class CoProcessorEx1 extends CoProcessor {
   def ex1 = new InstructionFunction[Cmd, Rsp](new Cmd(), new Rsp()) {
     // val pattern: String = s"0000001----------110-----${custom0}"
     val pattern: String = s"00000------------110-----${custom0}"
-    val description: String = "Example Coprocessor Float ADD"
+    val description: String = "Example Coprocessor Float SUB"
 
     def build(controller: EventController): Unit = {
 
@@ -90,306 +90,162 @@ class CoProcessorEx1 extends CoProcessor {
           }
         }
         
-        val result = new Area{
-            val sign = UInt(1 bits)
-            val exp = UInt(8 bits)
-            val mantissa = UInt(23 bits)
-            val manextend = UInt(25 bits)
+        val add = new Area{
+          val result = new Area{
+            val sign = Reg(UInt(1 bits))
+            val exp = Reg(UInt(8 bits))
+            val mantissa = Reg(UInt(23 bits))
+            val manextend = Reg(UInt(25 bits))
             
             val isZero = False
             val isInfinity = False
             val isNaN = False
-        }
-        
-        val add = new Area{
-            // exponent match
-            val expRS1GE = rs1.exp >= rs2.exp
-            val expDiff = UInt(8 bits)
-            val expMax = UInt(8 bits)
-            when(expRS1GE){
-                expDiff := rs1.exp - rs2.exp
-                expMax := rs1.exp + U(1, 8 bits)
-            }.otherwise{
-                expDiff := rs2.exp - rs1.exp
-                expMax := rs2.exp + U(1, 8 bits)
-            }
-
-            // 对阶
-            val MinFraction = UInt(24 bits)
-            val MaxFraction = UInt(24 bits)
-            when(expRS1GE){
-              result.sign := rs1.sign
-              MinFraction := rs2.manextend |>> expDiff
-              MaxFraction := rs1.manextend
-            }.otherwise{
-              result.sign := rs2.sign
-              MinFraction := rs1.manextend |>> expDiff
-              MaxFraction := rs2.manextend
-            }
-
-            // 尾数相加
-            val TempFraction = UInt(25 bits)
-            when(rs1.sign === rs2.sign){
-              TempFraction := ((B(0,1 bits) ## MaxFraction).asUInt + (B(0,1 bits) ## MinFraction).asUInt)
-            }.otherwise{
-              TempFraction := ((B(0,1 bits) ## MaxFraction).asUInt - (B(0,1 bits) ## MinFraction).asUInt)
-            }
-
-			// 规格化
-			val TempF_0 = TempFraction(24 downto 1)
-			val TempE_0 = expMax
-			val TempF_1 = UInt(24 bits)
-			val TempE_1 = UInt(8 bits)
-			when(!TempF_0(23)){
-				TempF_1 := TempF_0 |<< 1
-				TempE_1 := TempE_0 - U(1, 8 bits)
-			}.otherwise{
-				TempF_1 := TempF_0
-				TempE_1 := TempE_0
-			}
-			
-			val TempF_2 = UInt(24 bits)
-			val TempE_2 = UInt(8 bits)
-			when(!TempF_1(23)){
-				TempF_2 := TempF_1 |<< 1
-				TempE_2 := TempE_1 - U(1, 8 bits)
-			}.otherwise{
-				TempF_2 := TempF_1
-				TempE_2 := TempE_1
-			}
-
-			val TempF_3 = UInt(24 bits)
-			val TempE_3 = UInt(8 bits)
-			when(!TempF_2(23)){
-				TempF_3 := TempF_2 |<< 1
-				TempE_3 := TempE_2 - U(1, 8 bits)
-			}.otherwise{
-				TempF_3 := TempF_2
-				TempE_3 := TempE_2
-			}
-
-			val TempF_4 = UInt(24 bits)
-			val TempE_4 = UInt(8 bits)
-			when(!TempF_3(23)){
-				TempF_4 := TempF_3 |<< 1
-				TempE_4 := TempE_3 - U(1, 8 bits)
-			}.otherwise{
-				TempF_4 := TempF_3
-				TempE_4 := TempE_3
-			}
-
-			val TempF_5 = UInt(24 bits)
-			val TempE_5 = UInt(8 bits)
-			when(!TempF_4(23)){
-				TempF_5 := TempF_4 |<< 1
-				TempE_5 := TempE_4 - U(1, 8 bits)
-			}.otherwise{
-				TempF_5 := TempF_4
-				TempE_5 := TempE_4
-			}
-
-			val TempF_6 = UInt(24 bits)
-			val TempE_6 = UInt(8 bits)
-			when(!TempF_5(23)){
-				TempF_6 := TempF_5 |<< 1
-				TempE_6 := TempE_5 - U(1, 8 bits)
-			}.otherwise{
-				TempF_6 := TempF_5
-				TempE_6 := TempE_5
-			}
-
-			val TempF_7 = UInt(24 bits)
-			val TempE_7 = UInt(8 bits)
-			when(!TempF_6(23)){
-				TempF_7 := TempF_6 |<< 1
-				TempE_7 := TempE_6 - U(1, 8 bits)
-			}.otherwise{
-				TempF_7 := TempF_6
-				TempE_7 := TempE_6
-			}
-
-			val TempF_8 = UInt(24 bits)
-			val TempE_8 = UInt(8 bits)
-			when(!TempF_7(23)){
-				TempF_8 := TempF_7 |<< 1
-				TempE_8 := TempE_7 - U(1, 8 bits)
-			}.otherwise{
-				TempF_8 := TempF_7
-				TempE_8 := TempE_7
-			}
-
-			val TempF_9 = UInt(24 bits)
-			val TempE_9 = UInt(8 bits)
-			when(!TempF_8(23)){
-				TempF_9 := TempF_8 |<< 1
-				TempE_9 := TempE_8 - U(1, 8 bits)
-			}.otherwise{
-				TempF_9 := TempF_8
-				TempE_9 := TempE_8
-			}
-
-			val TempF_10 = UInt(24 bits)
-			val TempE_10 = UInt(8 bits)
-			when(!TempF_9(23)){
-				TempF_10 := TempF_9 |<< 1
-				TempE_10 := TempE_9 - U(1, 8 bits)
-			}.otherwise{
-				TempF_10 := TempF_9
-				TempE_10 := TempE_9
-			}
-
-			val TempF_11 = UInt(24 bits)
-			val TempE_11 = UInt(8 bits)
-			when(!TempF_10(23)){
-				TempF_11 := TempF_10 |<< 1
-				TempE_11 := TempE_10 - U(1, 8 bits)
-			}.otherwise{
-				TempF_11 := TempF_10
-				TempE_11 := TempE_10
-			}
-
-			val TempF_12 = UInt(24 bits)
-			val TempE_12 = UInt(8 bits)
-			when(!TempF_11(23)){
-				TempF_12 := TempF_11 |<< 1
-				TempE_12 := TempE_11 - U(1, 8 bits)
-			}.otherwise{
-				TempF_12 := TempF_11
-				TempE_12 := TempE_11
-			}
-
-			val TempF_13 = UInt(24 bits)
-			val TempE_13 = UInt(8 bits)
-			when(!TempF_12(23)){
-				TempF_13 := TempF_12 |<< 1
-				TempE_13 := TempE_12 - U(1, 8 bits)
-			}.otherwise{
-				TempF_13 := TempF_12
-				TempE_13 := TempE_12
-			}
-
-			val TempF_14 = UInt(24 bits)
-			val TempE_14 = UInt(8 bits)
-			when(!TempF_13(23)){
-				TempF_14 := TempF_13 |<< 1
-				TempE_14 := TempE_13 - U(1, 8 bits)
-			}.otherwise{
-				TempF_14 := TempF_13
-				TempE_14 := TempE_13
-			}
-
-			val TempF_15 = UInt(24 bits)
-			val TempE_15 = UInt(8 bits)
-			when(!TempF_14(23)){
-				TempF_15 := TempF_14 |<< 1
-				TempE_15 := TempE_14 - U(1, 8 bits)
-			}.otherwise{
-				TempF_15 := TempF_14
-				TempE_15 := TempE_14
-			}
-
-			val TempF_16 = UInt(24 bits)
-			val TempE_16 = UInt(8 bits)
-			when(!TempF_15(23)){
-				TempF_16 := TempF_15 |<< 1
-				TempE_16 := TempE_15 - U(1, 8 bits)
-			}.otherwise{
-				TempF_16 := TempF_15
-				TempE_16 := TempE_15
-			}
-
-			val TempF_17 = UInt(24 bits)
-			val TempE_17 = UInt(8 bits)
-			when(!TempF_16(23)){
-				TempF_17 := TempF_16 |<< 1
-				TempE_17 := TempE_16 - U(1, 8 bits)
-			}.otherwise{
-				TempF_17 := TempF_16
-				TempE_17 := TempE_16
-			}
-
-			val TempF_18 = UInt(24 bits)
-			val TempE_18 = UInt(8 bits)
-			when(!TempF_17(23)){
-				TempF_18 := TempF_17 |<< 1
-				TempE_18 := TempE_17 - U(1, 8 bits)
-			}.otherwise{
-				TempF_18 := TempF_17
-				TempE_18 := TempE_17
-			}
-
-			val TempF_19 = UInt(24 bits)
-			val TempE_19 = UInt(8 bits)
-			when(!TempF_18(23)){
-				TempF_19 := TempF_18 |<< 1
-				TempE_19 := TempE_18 - U(1, 8 bits)
-			}.otherwise{
-				TempF_19 := TempF_18
-				TempE_19 := TempE_18
-			}
-
-			val TempF_20 = UInt(24 bits)
-			val TempE_20 = UInt(8 bits)
-			when(!TempF_19(23)){
-				TempF_20 := TempF_19 |<< 1
-				TempE_20 := TempE_19 - U(1, 8 bits)
-			}.otherwise{
-				TempF_20 := TempF_19
-				TempE_20 := TempE_19
-			}
-
-			val TempF_21 = UInt(24 bits)
-			val TempE_21 = UInt(8 bits)
-			when(!TempF_20(23)){
-				TempF_21 := TempF_20 |<< 1
-				TempE_21 := TempE_20 - U(1, 8 bits)
-			}.otherwise{
-				TempF_21 := TempF_20
-				TempE_21 := TempE_20
-			}
-
-			val TempF_22 = UInt(24 bits)
-			val TempE_22 = UInt(8 bits)
-			when(!TempF_21(23)){
-				TempF_22 := TempF_21 |<< 1
-				TempE_22 := TempE_21 - U(1, 8 bits)
-			}.otherwise{
-				TempF_22 := TempF_21
-				TempE_22 := TempE_21
-			}
-
-			val TempF_23 = UInt(24 bits)
-			val TempE_23 = UInt(8 bits)
-			when(!TempF_22(23)){
-				TempF_23 := TempF_22 |<< 1
-				TempE_23 := TempE_22 - U(1, 8 bits)
-			}.otherwise{
-				TempF_23 := TempF_22
-				TempE_23 := TempE_22
-			}
-
-			// val TempF_24 = UInt(24 bits)
-			// val TempE_24 = UInt(8 bits)
-			// when(!TempF_23(23)){
-			// 	TempF_24 := TempF_23 |<< 1
-			// 	TempE_24 := TempE_23 - U(1, 8 bits)
-			// }.otherwise{
-			// 	TempF_24 := TempF_23
-			// 	TempE_24 := TempE_23
-			// }
-			
-			result.exp := TempE_23
-			result.mantissa := TempF_23(22 downto 0)
-            val data = result.sign.asBits ## result.exp.asBits ## result.mantissa.asBits
-        }
-        
-        switch(command.cpuinstruction(26 downto 25)) {
-          is(B"10") {
-            response.data := add.data
           }
-          default {
-            response.data := (RS1.asUInt + RS2.asUInt).asBits
+          val subRS1Sign = RegNext(rs1.sign)
+          val subRS2Sign = RegNext(rs2.sign)
+          // exponent match
+          val expRS1GE = rs1.exp >= rs2.exp
+          val expDiff = Reg(UInt(8 bits))
+          val expMax = Reg(UInt(8 bits))
+          when(expRS1GE){
+              expDiff := rs1.exp - rs2.exp
+              expMax := rs1.exp + U(1, 8 bits)
+          }.otherwise{
+              expDiff := rs2.exp - rs1.exp
+              expMax := rs2.exp + U(1, 8 bits)
           }
+
+          // 对阶
+          val MinFraction = Reg(UInt(24 bits))
+          val MaxFraction = Reg(UInt(24 bits))
+          when(expRS1GE){
+            result.sign := subRS1Sign
+            MinFraction := rs2.manextend |>> expDiff
+            MaxFraction := rs1.manextend
+          }.otherwise{
+            result.sign := subRS2Sign
+            MinFraction := rs1.manextend |>> expDiff
+            MaxFraction := rs2.manextend
+          }
+
+          // 尾数相加
+          val TempFraction = Reg(UInt(25 bits))
+          when(subRS1Sign === subRS2Sign){
+            TempFraction := ((B(0,1 bits) ## MaxFraction).asUInt + (B(0,1 bits) ## MinFraction).asUInt)
+          }.otherwise{
+            TempFraction := ((B(0,1 bits) ## MaxFraction).asUInt - (B(0,1 bits) ## MinFraction).asUInt)
+          }
+
+          // 规格化
+          val sel0 = B(0,8 bits) ## TempFraction(24 downto 1)
+          val sel1 = Bits(16 bits)
+          val sel2 = Bits(8 bits)
+          val sel3 = Bits(4 bits)
+          val sel4 = Bits(2 bits)
+          val pos = Bits(5 bits)
+          sel1 := Mux(sel0(31 downto 16).orR, sel0(31 downto 16), sel0(15 downto 0))
+          pos(4) := Mux(sel0(31 downto 16).orR, Bool(true), Bool(false))
+          sel2 := Mux(sel1(15 downto 8).orR, sel1(15 downto 8), sel1(7 downto 0))
+          pos(3) := Mux(sel1(15 downto 8).orR, Bool(true), Bool(false))
+          sel3 := Mux(sel2(7 downto 4).orR, sel2(7 downto 4), sel2(3 downto 0))
+          pos(2) := Mux(sel2(7 downto 4).orR, Bool(true), Bool(false))
+          sel4 := Mux(sel3(3 downto 2).orR, sel2(3 downto 2), sel2(1 downto 0))
+          pos(1) := Mux(sel3(3 downto 2).orR, Bool(true), Bool(false))
+          pos(0) := Mux(sel4(1), Bool(true), Bool(false))
+
+          val num = Reg(UInt(5 bits))
+          val NormF = Reg(UInt(24 bits))
+          val NormE = Reg(UInt(8 bits))
+          num := U(23,5 bits) - pos.asUInt
+          NormF := (sel0(23 downto 0) |<< num).asUInt
+          NormE := expMax - num
+    
+          result.exp := NormE
+          result.mantissa := NormF(22 downto 0)
+          val data = result.sign.asBits ## result.exp.asBits ## result.mantissa.asBits
+        }
+
+        val sub = new Area{
+          val result = new Area{
+            val sign = Reg(UInt(1 bits))
+            val exp = Reg(UInt(8 bits))
+            val mantissa = Reg(UInt(23 bits))
+            val manextend = Reg(UInt(25 bits))
+            
+            val isZero = False
+            val isInfinity = False
+            val isNaN = False
+          }
+          val subRS1Sign = RegNext(rs1.sign)
+          val subRS2Sign = RegNext(rs2.sign + U(1, 1 bits))
+          // exponent match
+          val expRS1GE = rs1.exp >= rs2.exp
+          val expDiff = Reg(UInt(8 bits))
+          val expMax = Reg(UInt(8 bits))
+          when(expRS1GE){
+              expDiff := rs1.exp - rs2.exp
+              expMax := rs1.exp + U(1, 8 bits)
+          }.otherwise{
+              expDiff := rs2.exp - rs1.exp
+              expMax := rs2.exp + U(1, 8 bits)
+          }
+
+          // 对阶
+          val MinFraction = Reg(UInt(24 bits))
+          val MaxFraction = Reg(UInt(24 bits))
+          when(expRS1GE){
+            result.sign := subRS1Sign
+            MinFraction := rs2.manextend |>> expDiff
+            MaxFraction := rs1.manextend
+          }.otherwise{
+            result.sign := subRS2Sign
+            MinFraction := rs1.manextend |>> expDiff
+            MaxFraction := rs2.manextend
+          }
+
+          // 尾数相加
+          val TempFraction = Reg(UInt(25 bits))
+          when(subRS1Sign === subRS2Sign){
+            TempFraction := ((B(0,1 bits) ## MaxFraction).asUInt + (B(0,1 bits) ## MinFraction).asUInt)
+          }.otherwise{
+            TempFraction := ((B(0,1 bits) ## MaxFraction).asUInt - (B(0,1 bits) ## MinFraction).asUInt)
+          }
+
+          // 规格化
+          val sel0 = B(0,8 bits) ## TempFraction(24 downto 1)
+          val sel1 = Bits(16 bits)
+          val sel2 = Bits(8 bits)
+          val sel3 = Bits(4 bits)
+          val sel4 = Bits(2 bits)
+          val pos = Bits(5 bits)
+          sel1 := Mux(sel0(31 downto 16).orR, sel0(31 downto 16), sel0(15 downto 0))
+          pos(4) := Mux(sel0(31 downto 16).orR, Bool(true), Bool(false))
+          sel2 := Mux(sel1(15 downto 8).orR, sel1(15 downto 8), sel1(7 downto 0))
+          pos(3) := Mux(sel1(15 downto 8).orR, Bool(true), Bool(false))
+          sel3 := Mux(sel2(7 downto 4).orR, sel2(7 downto 4), sel2(3 downto 0))
+          pos(2) := Mux(sel2(7 downto 4).orR, Bool(true), Bool(false))
+          sel4 := Mux(sel3(3 downto 2).orR, sel2(3 downto 2), sel2(1 downto 0))
+          pos(1) := Mux(sel3(3 downto 2).orR, Bool(true), Bool(false))
+          pos(0) := Mux(sel4(1), Bool(true), Bool(false))
+
+          val num = Reg(UInt(5 bits))
+          val NormF = Reg(UInt(24 bits))
+          val NormE = Reg(UInt(8 bits))
+          num := U(23,5 bits) - pos.asUInt
+          NormF := (sel0(23 downto 0) |<< num).asUInt
+          NormE := expMax - num
+    
+          result.exp := NormE
+          result.mantissa := NormF(22 downto 0)
+          val data = result.sign.asBits ## result.exp.asBits ## result.mantissa.asBits
+        }
+
+        when(command.cpuinstruction(26 downto 25) === B"10"){
+          response.data := add.data
+        }.elsewhen(command.cpuinstruction(26 downto 25) === B"11"){
+          response.data := sub.data
+        }.otherwise{
+          response.data := (RS1.asUInt + RS2.asUInt).asBits
         }
         
         when(counter.willOverflowIfInc) {
--- src/test/cpp/custom/rocc/build/custom_rocc.asm
+++ src/test/cpp/custom/rocc/build/custom_rocc.asm
@@ -5,29 +5,29 @@ build/custom_rocc.elf:     file format elf32-littleriscv
 Disassembly of section .crt_section:
 
 00000000 <_start>:
-   0:	00400e13          	li	t3,4
-   4:	010200b7          	lui	ra,0x1020
-   8:	30408093          	addi	ra,ra,772 # 1020304 <pass+0x1020298>
-   c:	02040137          	lui	sp,0x2040
-  10:	60810113          	addi	sp,sp,1544 # 2040608 <pass+0x204059c>
-  14:	0220e18b          	0x220e18b
-  18:	00100e13          	li	t3,1
-  1c:	3e4cd0b7          	lui	ra,0x3e4cd
-  20:	ccc08093          	addi	ra,ra,-820 # 3e4ccccc <pass+0x3e4ccc60>
-  24:	3eccd137          	lui	sp,0x3eccd
-  28:	ccc10113          	addi	sp,sp,-820 # 3ecccccc <pass+0x3ecccc60>
-  2c:	0420e18b          	0x420e18b
-  30:	00200e13          	li	t3,2
-  34:	bfc800b7          	lui	ra,0xbfc80
-  38:	c010a137          	lui	sp,0xc010a
-  3c:	3d710113          	addi	sp,sp,983 # c010a3d7 <pass+0xc010a36b>
-  40:	0420e18b          	0x420e18b
-  44:	00300e13          	li	t3,3
-  48:	be4cd0b7          	lui	ra,0xbe4cd
-  4c:	ccc08093          	addi	ra,ra,-820 # be4ccccc <pass+0xbe4ccc60>
-  50:	3f4cd137          	lui	sp,0x3f4cd
-  54:	ccc10113          	addi	sp,sp,-820 # 3f4ccccc <pass+0x3f4ccc60>
-  58:	0420e18b          	0x420e18b
+   0:	00100e13          	li	t3,1
+   4:	3e4cd0b7          	lui	ra,0x3e4cd
+   8:	ccc08093          	addi	ra,ra,-820 # 3e4ccccc <pass+0x3e4ccc60>
+   c:	3eccd137          	lui	sp,0x3eccd
+  10:	ccc10113          	addi	sp,sp,-820 # 3ecccccc <pass+0x3ecccc60>
+  14:	0420e18b          	0x420e18b
+  18:	00200e13          	li	t3,2
+  1c:	bfc800b7          	lui	ra,0xbfc80
+  20:	c010a137          	lui	sp,0xc010a
+  24:	3d710113          	addi	sp,sp,983 # c010a3d7 <pass+0xc010a36b>
+  28:	0620e18b          	0x620e18b
+  2c:	00300e13          	li	t3,3
+  30:	be4cd0b7          	lui	ra,0xbe4cd
+  34:	ccc08093          	addi	ra,ra,-820 # be4ccccc <pass+0xbe4ccc60>
+  38:	3f4cd137          	lui	sp,0x3f4cd
+  3c:	ccc10113          	addi	sp,sp,-820 # 3f4ccccc <pass+0x3f4ccc60>
+  40:	0620e18b          	0x620e18b
+  44:	00400e13          	li	t3,4
+  48:	010200b7          	lui	ra,0x1020
+  4c:	30408093          	addi	ra,ra,772 # 1020304 <pass+0x1020298>
+  50:	02040137          	lui	sp,0x2040
+  54:	60810113          	addi	sp,sp,1544 # 2040608 <pass+0x204059c>
+  58:	0220e18b          	0x220e18b
   5c:	0100006f          	j	6c <pass>
 
 00000060 <fail>:

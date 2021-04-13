bb78ceac82f4ee0ce8cd00ecbff25ffbd1a65a7f
GuitarZeiZei <213173648@seu.edu.cn>
2021-04-13 17:34:31 +0800

    Merge Add and SUB

--- src/main/scala/vexriscv/RoCC/CoProcessorEx1.scala
+++ src/main/scala/vexriscv/RoCC/CoProcessorEx1.scala
@@ -1,6 +1,6 @@
 /**
   * Created on 2021/3/24
-  * Instantiation of a Float Cal coprocessor
+  * Instantiation of a Folat Cal coprocessor
   */
 
 package vexriscv.RoCC
@@ -43,6 +43,13 @@ class CoProcessorEx1 extends CoProcessor {
 
         val RS1 = command.cpuRS1.noCombLoopCheck
         val RS2 = command.cpuRS2.noCombLoopCheck
+
+        val addorsub = UInt(1 bits)
+        when(command.cpuinstruction(26 downto 25) === B"10"){
+          addorsub := U(0,1 bits)  //add
+        }.otherwise{
+          addorsub := U(1,1 bits)  //sub
+        }
 
         val rs1 = new Area{
           // 原始符号位、指数、小数部分
@@ -90,7 +97,7 @@ class CoProcessorEx1 extends CoProcessor {
           }
         }
         
-        val add = new Area{
+        val addsub = new Area{
           val result = new Area{
             val sign = Reg(UInt(1 bits))
             val exp = Reg(UInt(8 bits))
@@ -101,8 +108,14 @@ class CoProcessorEx1 extends CoProcessor {
             val isInfinity = False
             val isNaN = False
           }
-          val subRS1Sign = RegNext(rs1.sign)
-          val subRS2Sign = RegNext(rs2.sign)
+          val addsubRS1Sign = Reg(UInt(1 bits))
+          val addsubRS2Sign = Reg(UInt(1 bits))
+          addsubRS1Sign := rs1.sign
+          when(addorsub === U(0,1 bits)){
+            addsubRS2Sign := rs2.sign
+          }.otherwise{
+            addsubRS2Sign := rs2.sign + U(1,1 bits)
+          }
           // exponent match
           val expRS1GE = rs1.exp >= rs2.exp
           val expDiff = Reg(UInt(8 bits))
@@ -119,93 +132,18 @@ class CoProcessorEx1 extends CoProcessor {
           val MinFraction = Reg(UInt(24 bits))
           val MaxFraction = Reg(UInt(24 bits))
           when(expRS1GE){
-            result.sign := subRS1Sign
+            result.sign := addsubRS2Sign
             MinFraction := rs2.manextend |>> expDiff
             MaxFraction := rs1.manextend
           }.otherwise{
-            result.sign := subRS2Sign
+            result.sign := addsubRS2Sign
             MinFraction := rs1.manextend |>> expDiff
             MaxFraction := rs2.manextend
           }
 
           // 尾数相加
           val TempFraction = Reg(UInt(25 bits))
-          when(subRS1Sign === subRS2Sign){
-            TempFraction := ((B(0,1 bits) ## MaxFraction).asUInt + (B(0,1 bits) ## MinFraction).asUInt)
-          }.otherwise{
-            TempFraction := ((B(0,1 bits) ## MaxFraction).asUInt - (B(0,1 bits) ## MinFraction).asUInt)
-          }
-
-          // 规格化
-          val sel0 = B(0,8 bits) ## TempFraction(24 downto 1)
-          val sel1 = Bits(16 bits)
-          val sel2 = Bits(8 bits)
-          val sel3 = Bits(4 bits)
-          val sel4 = Bits(2 bits)
-          val pos = Bits(5 bits)
-          sel1 := Mux(sel0(31 downto 16).orR, sel0(31 downto 16), sel0(15 downto 0))
-          pos(4) := Mux(sel0(31 downto 16).orR, Bool(true), Bool(false))
-          sel2 := Mux(sel1(15 downto 8).orR, sel1(15 downto 8), sel1(7 downto 0))
-          pos(3) := Mux(sel1(15 downto 8).orR, Bool(true), Bool(false))
-          sel3 := Mux(sel2(7 downto 4).orR, sel2(7 downto 4), sel2(3 downto 0))
-          pos(2) := Mux(sel2(7 downto 4).orR, Bool(true), Bool(false))
-          sel4 := Mux(sel3(3 downto 2).orR, sel2(3 downto 2), sel2(1 downto 0))
-          pos(1) := Mux(sel3(3 downto 2).orR, Bool(true), Bool(false))
-          pos(0) := Mux(sel4(1), Bool(true), Bool(false))
-
-          val num = Reg(UInt(5 bits))
-          val NormF = Reg(UInt(24 bits))
-          val NormE = Reg(UInt(8 bits))
-          num := U(23,5 bits) - pos.asUInt
-          NormF := (sel0(23 downto 0) |<< num).asUInt
-          NormE := expMax - num
-    
-          result.exp := NormE
-          result.mantissa := NormF(22 downto 0)
-          val data = result.sign.asBits ## result.exp.asBits ## result.mantissa.asBits
-        }
-
-        val sub = new Area{
-          val result = new Area{
-            val sign = Reg(UInt(1 bits))
-            val exp = Reg(UInt(8 bits))
-            val mantissa = Reg(UInt(23 bits))
-            val manextend = Reg(UInt(25 bits))
-            
-            val isZero = False
-            val isInfinity = False
-            val isNaN = False
-          }
-          val subRS1Sign = RegNext(rs1.sign)
-          val subRS2Sign = RegNext(rs2.sign + U(1, 1 bits))
-          // exponent match
-          val expRS1GE = rs1.exp >= rs2.exp
-          val expDiff = Reg(UInt(8 bits))
-          val expMax = Reg(UInt(8 bits))
-          when(expRS1GE){
-              expDiff := rs1.exp - rs2.exp
-              expMax := rs1.exp + U(1, 8 bits)
-          }.otherwise{
-              expDiff := rs2.exp - rs1.exp
-              expMax := rs2.exp + U(1, 8 bits)
-          }
-
-          // 对阶
-          val MinFraction = Reg(UInt(24 bits))
-          val MaxFraction = Reg(UInt(24 bits))
-          when(expRS1GE){
-            result.sign := subRS1Sign
-            MinFraction := rs2.manextend |>> expDiff
-            MaxFraction := rs1.manextend
-          }.otherwise{
-            result.sign := subRS2Sign
-            MinFraction := rs1.manextend |>> expDiff
-            MaxFraction := rs2.manextend
-          }
-
-          // 尾数相加
-          val TempFraction = Reg(UInt(25 bits))
-          when(subRS1Sign === subRS2Sign){
+          when(addsubRS2Sign === addsubRS2Sign){
             TempFraction := ((B(0,1 bits) ## MaxFraction).asUInt + (B(0,1 bits) ## MinFraction).asUInt)
           }.otherwise{
             TempFraction := ((B(0,1 bits) ## MaxFraction).asUInt - (B(0,1 bits) ## MinFraction).asUInt)
@@ -241,9 +179,9 @@ class CoProcessorEx1 extends CoProcessor {
         }
 
         when(command.cpuinstruction(26 downto 25) === B"10"){
-          response.data := add.data
+          response.data := addsub.data
         }.elsewhen(command.cpuinstruction(26 downto 25) === B"11"){
-          response.data := sub.data
+          response.data := addsub.data
         }.otherwise{
           response.data := (RS1.asUInt + RS2.asUInt).asBits
         }

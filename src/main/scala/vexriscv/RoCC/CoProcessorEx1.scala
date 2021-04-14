db8585fe9124a1916d97d9d10ca0a5614f16da0c
GuitarZeiZei <213173648@seu.edu.cn>
2021-04-14 11:18:32 +0800

    Update ADD SUB MUL

--- src/main/scala/vexriscv/RoCC/CoProcessorEx1.scala
+++ src/main/scala/vexriscv/RoCC/CoProcessorEx1.scala
@@ -30,7 +30,7 @@ class CoProcessorEx1 extends CoProcessor {
   def ex1 = new InstructionFunction[Cmd, Rsp](new Cmd(), new Rsp()) {
     // val pattern: String = s"0000001----------110-----${custom0}"
     val pattern: String = s"00000------------110-----${custom0}"
-    val description: String = "Example Coprocessor Float SUB"
+    val description: String = "Example Coprocessor Float Calculation"
 
     def build(controller: EventController): Unit = {
 
@@ -44,11 +44,11 @@ class CoProcessorEx1 extends CoProcessor {
         val RS1 = command.cpuRS1.noCombLoopCheck
         val RS2 = command.cpuRS2.noCombLoopCheck
 
-        val addorsub = UInt(1 bits)
-        when(command.cpuinstruction(26 downto 25) === B"10"){
-          addorsub := U(0,1 bits)  //add
+        val addorsub = Bool
+        when(command.cpuinstruction(26 downto 25) === B"00"){
+          addorsub := Bool(false)  //add
         }.otherwise{
-          addorsub := U(1,1 bits)  //sub
+          addorsub := Bool(true)  //sub
         }
 
         val rs1 = new Area{
@@ -98,90 +98,153 @@ class CoProcessorEx1 extends CoProcessor {
         }
         
         val addsub = new Area{
-          val result = new Area{
-            val sign = Reg(UInt(1 bits))
-            val exp = Reg(UInt(8 bits))
-            val mantissa = Reg(UInt(23 bits))
-            val manextend = Reg(UInt(25 bits))
-            
-            val isZero = False
-            val isInfinity = False
-            val isNaN = False
-          }
-          val addsubRS1Sign = Reg(UInt(1 bits))
-          val addsubRS2Sign = Reg(UInt(1 bits))
-          addsubRS1Sign := rs1.sign
-          when(addorsub === U(0,1 bits)){
-            addsubRS2Sign := rs2.sign
+          val Comp_enable = Bool
+          val operand_a = Bits(32 bits)
+          val operand_b = Bits(32 bits)
+          //for operations always operand_a must not be less than b_operand
+          when(RS1(30 downto 0).asUInt < RS2(30 downto 0).asUInt){
+            Comp_enable := Bool(true)
+            operand_a := RS2
+            operand_b := RS1
           }.otherwise{
-            addsubRS2Sign := rs2.sign + U(1,1 bits)
+            Comp_enable := Bool(false)
+            operand_a := RS1
+            operand_b := RS2
           }
-          // exponent match
-          val expRS1GE = rs1.exp >= rs2.exp
-          val expDiff = Reg(UInt(8 bits))
-          val expMax = Reg(UInt(8 bits))
-          when(expRS1GE){
-              expDiff := rs1.exp - rs2.exp
-              expMax := rs1.exp + U(1, 8 bits)
+          // val exp_a = operand_a(30 downto 23)
+          // val exp_b = operand_b(30 downto 23)
+
+          //Exception flag sets 1 if either one of the exponent is 255.
+          val Exception = (operand_a(30 downto 23).andR) | (operand_b(30 downto 23).andR)
+          val output_sign = UInt(1 bits)
+          when(addorsub){
+            when(Comp_enable){
+              output_sign := (!operand_a(31)).asUInt
+            }.otherwise{
+              output_sign := operand_a(31).asUInt
+            }
           }.otherwise{
-              expDiff := rs2.exp - rs1.exp
-              expMax := rs2.exp + U(1, 8 bits)
+            output_sign := operand_a(31).asUInt
           }
-
-          // 对阶
-          val MinFraction = Reg(UInt(24 bits))
-          val MaxFraction = Reg(UInt(24 bits))
-          when(expRS1GE){
-            result.sign := addsubRS2Sign
-            MinFraction := rs2.manextend |>> expDiff
-            MaxFraction := rs1.manextend
+          val operation_sub_addBar = Bool
+          when(addorsub){
+            operation_sub_addBar := operand_a(31) ^ operand_b(31)
           }.otherwise{
-            result.sign := addsubRS2Sign
-            MinFraction := rs1.manextend |>> expDiff
-            MaxFraction := rs2.manextend
+            operation_sub_addBar := !(operand_a(31) ^ operand_b(31))
           }
+          //Assigining significand values according to Hidden Bit.
+          //If exponent is equal to zero then hidden bit will be 0 for that respective significand else it will be 1
+          val significand_a = Mux(operand_a(30 downto 23).orR, B(1,1 bits)##operand_a(22 downto 0), B(0,1 bits)##operand_a(22 downto 0))
+          val significand_b = Mux(operand_b(30 downto 23).orR, B(1,1 bits)##operand_b(22 downto 0), B(0,1 bits)##operand_b(22 downto 0))
+          //Evaluating Exponent Difference
+          val exponent_diff = operand_a(30 downto 23).asUInt - operand_b(30 downto 23).asUInt
+          //Shifting significand_b according to exponent_diff
+          val significand_b_add_sub = significand_b |>> exponent_diff
+          val exponent_b_add_sub = operand_b(30 downto 23).asUInt + exponent_diff
 
-          // 尾数相加
-          val TempFraction = Reg(UInt(25 bits))
-          when(addsubRS2Sign === addsubRS2Sign){
-            TempFraction := ((B(0,1 bits) ## MaxFraction).asUInt + (B(0,1 bits) ## MinFraction).asUInt)
+          //Checking exponents are same or not
+          val perform = operand_a(30 downto 23) === exponent_b_add_sub.asBits
+
+          ///////////////////////////////////////////////////////////////////////////////////////////////////////
+          //------------------------------------------------ADD BLOCK------------------------------------------//
+          val significand_add = Mux(perform & operation_sub_addBar, significand_a.asUInt.resize(25) + significand_b_add_sub.asUInt.resize(25), U(0,25 bits))
+          
+          //Result will be equal to Most 23 bits if carry generates else it will be Least 22 bits.
+          val add_sum = UInt(31 bits)
+          add_sum(22 downto 0) := Mux(significand_add(24), significand_add(23 downto 1), significand_add(22 downto 0))
+          
+          //If carry generates in sum value then exponent must be added with 1 else feed as it is.
+          add_sum(30 downto 23) := Mux(significand_add(24),U(1,8 bits)+operand_a(30 downto 23).asUInt,operand_a(30 downto 23).asUInt)
+          
+
+          ///////////////////////////////////////////////////////////////////////////////////////////////////////
+          //------------------------------------------------SUB BLOCK------------------------------------------//
+          val significand_sub_complement = Mux(perform & !operation_sub_addBar, (~significand_b_add_sub).asUInt+U(1,24 bits), U(0,24 bits))
+          val significand_sub = Mux(perform, significand_a.asUInt.resize(25) + significand_sub_complement.resize(25), U(0,25 bits))
+
+          // subnormlize
+          val shift = Reg(UInt(5 bits))
+          val subtraction_diff = Reg(UInt(25 bits))
+          val exponent_sub = Reg(UInt(8 bits))
+          when(!significand_sub(24)){
+            subtraction_diff := ~significand_sub + U(1,25 bits)
+            shift := U(0,5 bits)
+          }.elsewhen(!(significand_sub(23 downto 0).orR)){
+            subtraction_diff := significand_sub |<< 24
+            shift := U(24,5 bits)
           }.otherwise{
-            TempFraction := ((B(0,1 bits) ## MaxFraction).asUInt - (B(0,1 bits) ## MinFraction).asUInt)
+            val sel0 = B(0,8 bits) ## significand_sub(23 downto 0)
+            // val sel0 = B(0,8 bits) ## significand_sub(24 downto 1)
+            val sel1 = Bits(16 bits)
+            val sel2 = Bits(8 bits)
+            val sel3 = Bits(4 bits)
+            val sel4 = Bits(2 bits)
+            val pos = Bits(5 bits)
+            sel1 := Mux(sel0(31 downto 16).orR, sel0(31 downto 16), sel0(15 downto 0))
+            pos(4) := Mux(sel0(31 downto 16).orR, Bool(true), Bool(false))
+            sel2 := Mux(sel1(15 downto 8).orR, sel1(15 downto 8), sel1(7 downto 0))
+            pos(3) := Mux(sel1(15 downto 8).orR, Bool(true), Bool(false))
+            sel3 := Mux(sel2(7 downto 4).orR, sel2(7 downto 4), sel2(3 downto 0))
+            pos(2) := Mux(sel2(7 downto 4).orR, Bool(true), Bool(false))
+            sel4 := Mux(sel3(3 downto 2).orR, sel3(3 downto 2), sel3(1 downto 0))
+            pos(1) := Mux(sel3(3 downto 2).orR, Bool(true), Bool(false))
+            pos(0) := Mux(sel4(1), Bool(true), Bool(false))
+            shift := U(23,5 bits) - pos.asUInt
+            subtraction_diff := significand_sub |<< shift
           }
-
-          // 规格化
-          val sel0 = B(0,8 bits) ## TempFraction(24 downto 1)
-          val sel1 = Bits(16 bits)
-          val sel2 = Bits(8 bits)
-          val sel3 = Bits(4 bits)
-          val sel4 = Bits(2 bits)
-          val pos = Bits(5 bits)
-          sel1 := Mux(sel0(31 downto 16).orR, sel0(31 downto 16), sel0(15 downto 0))
-          pos(4) := Mux(sel0(31 downto 16).orR, Bool(true), Bool(false))
-          sel2 := Mux(sel1(15 downto 8).orR, sel1(15 downto 8), sel1(7 downto 0))
-          pos(3) := Mux(sel1(15 downto 8).orR, Bool(true), Bool(false))
-          sel3 := Mux(sel2(7 downto 4).orR, sel2(7 downto 4), sel2(3 downto 0))
-          pos(2) := Mux(sel2(7 downto 4).orR, Bool(true), Bool(false))
-          sel4 := Mux(sel3(3 downto 2).orR, sel2(3 downto 2), sel2(1 downto 0))
-          pos(1) := Mux(sel3(3 downto 2).orR, Bool(true), Bool(false))
-          pos(0) := Mux(sel4(1), Bool(true), Bool(false))
-
-          val num = Reg(UInt(5 bits))
-          val NormF = Reg(UInt(24 bits))
-          val NormE = Reg(UInt(8 bits))
-          num := U(23,5 bits) - pos.asUInt
-          NormF := (sel0(23 downto 0) |<< num).asUInt
-          NormE := expMax - num
-    
-          result.exp := NormE
-          result.mantissa := NormF(22 downto 0)
-          val data = result.sign.asBits ## result.exp.asBits ## result.mantissa.asBits
+          exponent_sub := operand_a(30 downto 23).asUInt - shift
+          val sub_diff = UInt(31 bits)
+          sub_diff(30 downto 23) := exponent_sub
+          sub_diff(22 downto 0) := subtraction_diff(22 downto 0)
+          val result = Bits(32 bits)
+          when(Exception){
+            result := B(0,32 bits)
+          }.elsewhen(!operation_sub_addBar){
+            result := output_sign ## sub_diff.asBits
+          }.otherwise{
+            result := output_sign ## add_sum.asBits
+          }
         }
 
-        when(command.cpuinstruction(26 downto 25) === B"10"){
-          response.data := addsub.data
-        }.elsewhen(command.cpuinstruction(26 downto 25) === B"11"){
-          response.data := addsub.data
+        val mul = new Area{
+          val exception = rs1.exp.andR || rs2.exp.andR
+          val zero = rs1.isZero | rs2.isZero
+
+          val sign = rs1.sign ^ rs2.sign
+
+          val preTempOut = UInt(48 bits)
+          preTempOut := rs1.manextend * rs2.manextend
+          val preExp = Mux(preTempOut(47), U(1,1 bits), U(0, 1 bits))
+          val tempout = Mux(preExp.msb, preTempOut, preTempOut |<< 1)
+          val rounded = tempout(22 downto 0).orR
+          val mantissa = tempout(46 downto 24) + ((tempout(23) & rounded).asUInt).resized
+          val expSum = UInt(9 bits)
+          val tempExp = UInt(9 bits)
+          tempExp := (B(0,1 bits) ## rs1.exp.asBits).asUInt + (B(0,1 bits) ## rs2.exp.asBits).asUInt
+          expSum := tempExp - U(127,8 bits) + preExp
+          val overFlow = ((expSum(8) & !expSum(7)) & !zero)
+          val underFlow = Mux(((expSum(8) & expSum(7)) & !zero), Bool(true), Bool(false))
+          
+          val result = Bits(32 bits)
+          when(exception){
+            result := B(0,32 bits)
+          }.elsewhen(zero){
+            result := sign.asBits ## B(0,31 bits)
+          }.elsewhen(overFlow){
+            result := sign.asBits ## B(255,8 bits) ## B(0,23 bits)
+          }.elsewhen(underFlow){
+            result := sign.asBits ## B(0,31 bits)
+          }.otherwise{
+            result := sign.asBits ## expSum(7 downto 0).asBits ## mantissa.asBits
+          }
+		    }
+
+        when(command.cpuinstruction(26 downto 25) === B"00"){
+          response.data := addsub.result
+        }.elsewhen(command.cpuinstruction(26 downto 25) === B"01"){
+          response.data := addsub.result
+        }.elsewhen(command.cpuinstruction(26 downto 25) === B"10"){
+          response.data := mul.result
         }.otherwise{
           response.data := (RS1.asUInt + RS2.asUInt).asBits
         }
--- src/test/cpp/custom/rocc/build/custom_rocc.asm
+++ src/test/cpp/custom/rocc/build/custom_rocc.asm
@@ -7,41 +7,68 @@ Disassembly of section .crt_section:
 00000000 <_start>:
    0:	00100e13          	li	t3,1
    4:	3e4cd0b7          	lui	ra,0x3e4cd
-   8:	ccc08093          	addi	ra,ra,-820 # 3e4ccccc <pass+0x3e4ccc60>
+   8:	ccc08093          	addi	ra,ra,-820 # 3e4ccccc <pass+0x3e4ccbf4>
    c:	3eccd137          	lui	sp,0x3eccd
-  10:	ccc10113          	addi	sp,sp,-820 # 3ecccccc <pass+0x3ecccc60>
-  14:	0420e18b          	0x420e18b
-  18:	00200e13          	li	t3,2
-  1c:	bfc800b7          	lui	ra,0xbfc80
-  20:	c010a137          	lui	sp,0xc010a
-  24:	3d710113          	addi	sp,sp,983 # c010a3d7 <pass+0xc010a36b>
-  28:	0620e18b          	0x620e18b
-  2c:	00300e13          	li	t3,3
-  30:	be4cd0b7          	lui	ra,0xbe4cd
-  34:	ccc08093          	addi	ra,ra,-820 # be4ccccc <pass+0xbe4ccc60>
-  38:	3f4cd137          	lui	sp,0x3f4cd
-  3c:	ccc10113          	addi	sp,sp,-820 # 3f4ccccc <pass+0x3f4ccc60>
-  40:	0620e18b          	0x620e18b
-  44:	00400e13          	li	t3,4
-  48:	010200b7          	lui	ra,0x1020
-  4c:	30408093          	addi	ra,ra,772 # 1020304 <pass+0x1020298>
-  50:	02040137          	lui	sp,0x2040
-  54:	60810113          	addi	sp,sp,1544 # 2040608 <pass+0x204059c>
-  58:	0220e18b          	0x220e18b
-  5c:	0100006f          	j	6c <pass>
+  10:	ccc10113          	addi	sp,sp,-820 # 3ecccccc <pass+0x3ecccbf4>
+  14:	0020e18b          	0x20e18b
+  18:	3f19a237          	lui	tp,0x3f19a
+  1c:	99920213          	addi	tp,tp,-1639 # 3f199999 <pass+0x3f1998c1>
+  20:	0a321663          	bne	tp,gp,cc <fail>
+  24:	00200e13          	li	t3,2
+  28:	bfc800b7          	lui	ra,0xbfc80
+  2c:	c010a137          	lui	sp,0xc010a
+  30:	3d710113          	addi	sp,sp,983 # c010a3d7 <pass+0xc010a2ff>
+  34:	0220e18b          	0x220e18b
+  38:	00300e13          	li	t3,3
+  3c:	be4cd0b7          	lui	ra,0xbe4cd
+  40:	ccc08093          	addi	ra,ra,-820 # be4ccccc <pass+0xbe4ccbf4>
+  44:	3f4cd137          	lui	sp,0x3f4cd
+  48:	ccc10113          	addi	sp,sp,-820 # 3f4ccccc <pass+0x3f4ccbf4>
+  4c:	0420e18b          	0x420e18b
+  50:	00400e13          	li	t3,4
+  54:	010200b7          	lui	ra,0x1020
+  58:	30408093          	addi	ra,ra,772 # 1020304 <pass+0x102022c>
+  5c:	02040137          	lui	sp,0x2040
+  60:	60810113          	addi	sp,sp,1544 # 2040608 <pass+0x2040530>
+  64:	0620e18b          	0x620e18b
+  68:	00500e13          	li	t3,5
+  6c:	123450b7          	lui	ra,0x12345
+  70:	67808093          	addi	ra,ra,1656 # 12345678 <pass+0x123455a0>
+  74:	87654137          	lui	sp,0x87654
+  78:	32110113          	addi	sp,sp,801 # 87654321 <pass+0x87654249>
+  7c:	0620e18b          	0x620e18b
+  80:	00600e13          	li	t3,6
+  84:	bfc800b7          	lui	ra,0xbfc80
+  88:	c010a137          	lui	sp,0xc010a
+  8c:	3d710113          	addi	sp,sp,983 # c010a3d7 <pass+0xc010a2ff>
+  90:	0420e18b          	0x420e18b
+  94:	00700e13          	li	t3,7
+  98:	be4cd0b7          	lui	ra,0xbe4cd
+  9c:	ccc08093          	addi	ra,ra,-820 # be4ccccc <pass+0xbe4ccbf4>
+  a0:	3f4cd137          	lui	sp,0x3f4cd
+  a4:	ccc10113          	addi	sp,sp,-820 # 3f4ccccc <pass+0x3f4ccbf4>
+  a8:	0220e18b          	0x220e18b
+  ac:	00800e13          	li	t3,8
+  b0:	41bba0b7          	lui	ra,0x41bba
+  b4:	99908093          	addi	ra,ra,-1639 # 41bb9999 <pass+0x41bb98c1>
+  b8:	41753137          	lui	sp,0x41753
+  bc:	f1a10113          	addi	sp,sp,-230 # 41752f1a <pass+0x41752e42>
+  c0:	0020e18b          	0x20e18b
+  c4:	0220e18b          	0x220e18b
+  c8:	0100006f          	j	d8 <pass>
 
-00000060 <fail>:
-  60:	f0100137          	lui	sp,0xf0100
-  64:	f2410113          	addi	sp,sp,-220 # f00fff24 <pass+0xf00ffeb8>
-  68:	01c12023          	sw	t3,0(sp)
+000000cc <fail>:
+  cc:	f0100137          	lui	sp,0xf0100
+  d0:	f2410113          	addi	sp,sp,-220 # f00fff24 <pass+0xf00ffe4c>
+  d4:	01c12023          	sw	t3,0(sp)
 
-0000006c <pass>:
-  6c:	f0100137          	lui	sp,0xf0100
-  70:	f2010113          	addi	sp,sp,-224 # f00fff20 <pass+0xf00ffeb4>
-  74:	00012023          	sw	zero,0(sp)
-  78:	00000013          	nop
-  7c:	00000013          	nop
-  80:	00000013          	nop
-  84:	00000013          	nop
-  88:	00000013          	nop
-  8c:	00000013          	nop
+000000d8 <pass>:
+  d8:	f0100137          	lui	sp,0xf0100
+  dc:	f2010113          	addi	sp,sp,-224 # f00fff20 <pass+0xf00ffe48>
+  e0:	00012023          	sw	zero,0(sp)
+  e4:	00000013          	nop
+  e8:	00000013          	nop
+  ec:	00000013          	nop
+  f0:	00000013          	nop
+  f4:	00000013          	nop
+  f8:	00000013          	nop
